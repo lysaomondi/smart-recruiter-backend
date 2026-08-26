7cab20a4547477aca836384cf05e7c38f939ad16
Sahal Mohamed <sahal.mohamed1@student.moringaschool.com>
2026-08-26 14:57:01 +0300

    register Codewars kata admin

--- integrations/admin.py
+++ integrations/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import CodewarsKata
+
+
+@admin.register(CodewarsKata)
+class CodewarsKataAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "kata_id",
+        "category",
+        "rank_name",
+        "fetched_at",
+    )
+
+    search_fields = (
+        "name",
+        "kata_id",
+        "slug",
+    )
+
+    list_filter = (
+        "category",
+        "rank_name",
+    )
